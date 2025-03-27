50ff1c7c5b7ab835c9cccd58a7f5f851479883b1
huajiezhang <huajiezhang@microsoft.com>
2025-03-27 11:28:49 +0800

    feat: message

--- packages/cli/src/commands/index.ts
+++ packages/cli/src/commands/index.ts
@@ -8,7 +8,7 @@ export async function start(binName: "teamsfx" | "teamsapp" = "teamsapp"): Promi
   rootCommand.name = binName;
   rootCommand.fullName = binName;
   logger.warning(
-    `Deprecation Warning: The CLI package "@microsoft/teamsapp-cli" is being renamed to "@microsoft/m365agentstoolkit-cli". The command name "teamsapp" will also change to "m365agents" accordingly in the upcoming release. The existing package will no longer receive updates. To stay up to date, please switch to the new package and update your workflows accordingly once it is available. `
+    `Deprecation Warning: The CLI package "@microsoft/teamsapp-cli" is being renamed to "@microsoft/m365agentstoolkit-cli". The command name "teamsapp" will also be renamed to "m365agents" accordingly in the upcoming release. The existing package will no longer receive updates. To stay up to date, please switch to the new package and update your workflows accordingly once it is available. `
   );
   await engine.start(rootCommand);
   // process.exit(0);
