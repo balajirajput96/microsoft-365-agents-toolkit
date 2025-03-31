a6718774c8809b41fdb30b8d75a039e35e02a2b2
Huajie Zhang <huajiezhang@microsoft.com>
2025-03-31 15:28:01 +0800

    feat: hardcode mos api endpoint and scope (#13568)

--- packages/fx-core/src/component/m365/serviceConstant.ts
+++ packages/fx-core/src/component/m365/serviceConstant.ts
@@ -1,8 +1,8 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
-export const MosServiceEndpoint = "{{SERVICE_ENDPOINT_PLACEHOLDER}}";
-export const MosServiceScope = "{{SERVICE_SCOPE_PLACEHOLDER}}";
+export const MosServiceEndpoint = "https://titles.prod.mos.microsoft.com";
+export const MosServiceScope = "https://titles.prod.mos.microsoft.com/.default";
 
 export interface MOS3Api {
   key: string;
