44b53005a3db20958adb3fc8f4ee627acf7be83e
Huajie Zhang <huajiezhang@microsoft.com>
2025-04-27 10:44:51 +0800

    refactor: add script to convert schema to ts (#13787)
    * refactor: convert script
    
    * fix: pnpm-lock.yaml

--- packages/manifest/convert.js
+++ packages/manifest/convert.js
@@ -1,79 +1,117 @@
+const {
+  quicktype,
+  InputData,
+  JSONSchemaInput,
+  FetchingJSONSchemaStore,
+  tsFlowOptions,
+  javaScriptOptions,
+} = require("quicktype-core-jayzhang");
 const fs = require("fs-extra");
 const path = require("path");
-const { exec } = require("child_process");
 
-async function convert(srcFolder, srcFileName, destFilePath) {
-  const command = `npx quicktype -s schema ${srcFileName} -o ${destFilePath} --prefer-const-values --prefer-unions`;
-  console.log(`Running command: ${command} in ${srcFolder}`);
-  return new Promise((resolve) => {
-    exec(command, { cwd: srcFolder }, (error, stdout, stderr) => {
-      if (error) {
-        console.error(`Error generating TypeScript for ${srcFileName}:`, error);
-        return;
-      }
-      if (stderr) {
-        console.error(`stderr for ${srcFileName}:`, stderr);
-      }
-      console.log(`Generated TypeScript file: ${destFilePath}`);
-      resolve();
-    });
+const manifestProjectFolder = __dirname;
+
+async function quicktypeJSONSchema(targetLanguage, typeName, jsonSchemaString) {
+  const schemaInput = new JSONSchemaInput(new FetchingJSONSchemaStore());
+  await schemaInput.addSource({ name: typeName, schema: jsonSchemaString });
+
+  const inputData = new InputData();
+  inputData.addInput(schemaInput);
+
+  tsFlowOptions.preferUnions.getValue = () => true;
+  tsFlowOptions.preferConstValues.getValue = () => true;
+  javaScriptOptions.jsonStringifySpaceNum.getValue = () => "4";
+
+  return await quicktype({
+    inputData,
+    lang: targetLanguage,
+    allPropertiesOptional: false,
+    alphabetizeProperties: false,
   });
 }
 
+async function convert(srcFolder, srcFileName, typeName, destFilePath) {
+  const jsonSchemaString = await fs.readFile(path.join(srcFolder, srcFileName), "utf-8");
+  const { lines } = await quicktypeJSONSchema("typescript", typeName, jsonSchemaString);
+  await fs.writeFile(destFilePath, lines.join("\n"), "utf-8");
+  console.log(`Generated ${destFilePath} from ${srcFileName}`);
+}
+
 async function generateTeamsManifestTypeFiles() {
-  const schemaFolder = path.join(__dirname, "src", "json-schemas", "teams");
-  const outputFolder = path.join(__dirname, "src", "generated-types", "teams");
+  const schemaFolder = path.join(manifestProjectFolder, "src", "json-schemas", "teams");
+  const outputFolder = path.join(manifestProjectFolder, "src", "generated-types", "teams");
+
   if (!(await fs.pathExists(outputFolder))) {
     await fs.ensureDir(outputFolder);
   }
+
   const versions = await fs.readdir(schemaFolder);
   for (const version of versions) {
     const convertedVersion = version.replace(".", "D").replace("v", "V");
     const schemaDir = path.join(schemaFolder, version);
     const schemaFile = "MicrosoftTeams.schema.json";
     const outputFileName = `TeamsManifest${convertedVersion}.ts`;
+    const outputTypeName = `TeamsManifest${convertedVersion}`;
     const outputPath = path.join(outputFolder, outputFileName);
-    await convert(schemaDir, schemaFile, outputPath);
+    await convert(schemaDir, schemaFile, outputTypeName, outputPath);
   }
 }
 
 async function generateDAManifestTypeFiles() {
-  const schemaFolder = path.join(__dirname, "src", "json-schemas", "copilot", "declarative-agent");
+  const schemaFolder = path.join(
+    manifestProjectFolder,
+    "src",
+    "json-schemas",
+    "copilot",
+    "declarative-agent"
+  );
   const outputFolder = path.join(
-    __dirname,
+    manifestProjectFolder,
     "src",
     "generated-types",
     "copilot",
     "declarative-agent"
   );
+
   if (!(await fs.pathExists(outputFolder))) {
     await fs.ensureDir(outputFolder);
   }
+
   const versions = await fs.readdir(schemaFolder);
   for (const version of versions) {
     const convertedVersion = version.replace(".", "D").replace("v", "V");
     const schemaDir = path.join(schemaFolder, version);
     const schemaFile = "schema.json";
     const outputFileName = `DeclarativeAgentManifest${convertedVersion}.ts`;
+    const outputTypeName = `DeclarativeAgentManifest${convertedVersion}`;
     const outputPath = path.join(outputFolder, outputFileName);
-    await convert(schemaDir, schemaFile, outputPath);
+    await convert(schemaDir, schemaFile, outputTypeName, outputPath);
   }
 }
 
 async function generatePluginManifestTypeFiles() {
-  const schemaFolder = path.join(__dirname, "src", "json-schemas", "copilot", "plugin");
-  const outputFolder = path.join(__dirname, "src", "generated-types", "copilot", "plugin");
+  const schemaFolder = path.join(manifestProjectFolder, "src", "json-schemas", "copilot", "plugin");
+  const outputFolder = path.join(
+    manifestProjectFolder,
+    "src",
+    "generated-types",
+    "copilot",
+    "plugin"
+  );
+
   if (!(await fs.pathExists(outputFolder))) {
     await fs.ensureDir(outputFolder);
   }
+
   const versions = await fs.readdir(schemaFolder);
   for (const version of versions) {
     const convertedVersion = version.replace(".", "D").replace("v", "V");
     const schemaDir = path.join(schemaFolder, version);
     const schemaFile = "schema.json";
     const outputFileName = `ApiPluginManifest${convertedVersion}.ts`;
+    const outputTypeName = `ApiPluginManifest${convertedVersion}`;
     const outputPath = path.join(outputFolder, outputFileName);
-    await convert(schemaDir, schemaFile, outputPath);
+    await convert(schemaDir, schemaFile, outputTypeName, outputPath);
   }
 }
 
