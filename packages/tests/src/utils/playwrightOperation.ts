d4a083ca4ae4ee08f4a22764d5fff2b136a2b07e
Helly Zhang <49181894+hellyzh@users.noreply.github.com>
2025-08-14 11:32:34 +0800

    test: update da copilot ui element validation (#14409)

--- packages/tests/src/utils/playwrightOperation.ts
+++ packages/tests/src/utils/playwrightOperation.ts
@@ -1744,7 +1744,7 @@ export async function validatePrompt(
     await page.waitForTimeout(Timeout.shortTimeLoading);
     console.log("start to verify prompt");
     const contenteditableSpan = await page?.waitForSelector(
-      'span[aria-label="Message Copilot"]'
+      'span[aria-label="Chat Input"]'
     );
     await contenteditableSpan?.click();
     await contenteditableSpan.fill(options?.prompt || "list repairs");
